e7216b78c70c99b10d65fbfb977510cb0d4e2146
baohan <baohanddd@gmail.com>
2024-07-31 14:42:22 +0800

    添加纯真IP库

--- example4.php
+++ example4.php
@@ -3,19 +3,18 @@ include "./vendor/autoload.php";
 
 use Fu\Geo\Service\Ip\IpGeoLocationService;
 
-$key = "QAQBZ-W6WC4-7GXUY-XF4RA-GXRW7-Y2BRX";
-
 $latitude  = 29.60001;
 $longitude = 91.00001;
 
 // example of cache
-$ip = "174.37.243.85";   // 美国 弗吉尼亚 阿什本 SoftLayer
 $ip = "198.27.124.186";  // 加拿大 魁北克 博阿努瓦 OVH
 $ip = "193.47.102.45";   // 沙特阿拉伯
-$ip = "31.13.94.7";      // 阿根廷 布宜诺斯艾利斯
 $ip = "185.60.216.11";   // 德国 黑森 法兰克福
-$ip = "110.191.253.233"; // chengdu
 $ip = "254.255.253.233";   // unknown
+$ip = "174.37.243.85";   // 美国 弗吉尼亚 阿什本 SoftLayer
+$ip = "31.13.94.7";      // 阿根廷 布宜诺斯艾利斯
+$ip = "110.191.253.233"; // chengdu
+$ip = "36.22.1.112";     // 浙江
 $service = new IpGeoLocationService();
 $response = $service->getLocation($ip);
 
